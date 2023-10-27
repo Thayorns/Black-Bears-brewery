d6b71f73316b90c00a40d3aed1f0de94887e3fa9
Thayorn <thayornSwordsMan@gmail.com>
2023-10-27 22:54:35 +0300

    home component styling & useScroll hook for nav

--- src/components/main/home/home.js
+++ src/components/main/home/home.js
@@ -1,12 +1,28 @@
-import { motion } from "framer-motion";
+import { motion, useTransform } from 'framer-motion';
+
+
 import './home.css'
 
 const Home = () => {
-
+    
 
     return (
-        <motion.div>
-             {/* Код для компонента */}
+        <motion.div className="home"
+            key='beers'
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -10, opacity: 0 }}
+            transition={{ duration: 0.2 }}
+        >
+            <motion.div className="logo-wrapper">
+                
+                <motion.div className="img-logo-wrapper">
+                    <img src={require('../../../images/bigLogo.jpg')}  alt="Black Bears brewery logo"/>
+                </motion.div>
+                <motion.div className="brewery-span-wrapper">
+                    <span className="brewery-span">We brew beer for You and ourselves.<br/>Brew till it gains a soul and meaning.</span>
+                </motion.div>
+            </motion.div>
         </motion.div>
     )
 }
--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,32 +1,25 @@
-import { motion, AnimatePresence } from "framer-motion"
+import { motion ,useScroll } from "framer-motion"
 import { useState } from "react"
+import { Avatar } from 'antd'
+
 import './nav.css'
 
 const Nav = () => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleMenu = () => {
-      setIsOpen(!isOpen);
-    };
-  
+    const avatar = <img src={require('../../images/smallLogo.png')}  alt="Black Bears brewery logo"/>
+    const { scrollYProgress } = useScroll()
+    
     return (
-      <motion.nav className="nav">
-        {/* <button onClick={toggleMenu}>Toggle Menu</button> */}
-        <img src={require('../../images/bigLogo.jpg')}  alt="Black Bears brewery logo"/>
-        {/* <AnimatePresence>
-          {isOpen && (
-            <motion.ul
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-            >
-              <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>Our Beer</motion.li>
-              <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>Contacts</motion.li>
-              <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>Gallery</motion.li>
-            </motion.ul>
-          )}
-        </AnimatePresence> */}
-      </motion.nav>
+      <motion.div>
+        
+        <motion.nav className="nav">
+          <Avatar size={90} icon={avatar}/>
+        </motion.nav>
+        
+        <motion.div
+            className="progress-bar"
+            style={{ scaleX: scrollYProgress }}
+        />
+      </motion.div>
     );
 }
 
