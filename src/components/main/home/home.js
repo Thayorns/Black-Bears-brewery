5a4035ea4aeb20472e427d059ec8d065f6770d68
Thayorn <thayornSwordsMan@gmail.com>
2023-11-27 14:26:18 +0300

    draggable items

--- src/components/main/home/home.js
+++ src/components/main/home/home.js
@@ -1,5 +1,5 @@
 import { motion, useDragControls } from 'framer-motion';
-
+import Draggable from 'react-draggable';
 
 import './home.css'
 
@@ -19,31 +19,28 @@ const Home = () => {
             transition={{ duration: 0.6 }}
         >  
             <motion.div className='home-brewery-section'>
-                <motion.h1 initial={{x: -100, opacity: 0}}
+                <motion.h1 initial={{x: -300, opacity: 0}}
                     animate={{ x: 0, opacity: 1 }}
-                    transition={{ delay: 1.0, type: "spring" }}
+                    transition={{ delay: 1.4, type: "spring" }}
                 >Welcome to the Black Bears Brewery!
                 </motion.h1>
-                <motion.p>Where every glass holds a story, every sip is an adventure, and every guest is a cherished part of our brewing family.</motion.p>
+                <motion.h2>Where every glass holds a story, every sip is an adventure, and every guest is a cherished part of our brewing family.</motion.h2>
             </motion.div> 
             
             <motion.div className='home-reposts-section'
                 onPointerDown={startDrag}>
                 
+                <Draggable>
+                    <img className='draggable-cards' src={require('../../../images/1stDragable.jpg')} alt='draggable card' />
+                </Draggable>
                 <motion.img 
-                    drag="x" 
+                    drag="x y" 
                     dragControls={controls}
                     className='draggable-cards' 
                     src={require('../../../images/1stDragable.jpg')}>
                 </motion.img>
                 <motion.img 
-                    drag="x" 
-                    dragControls={controls}
-                    className='draggable-cards' 
-                    src={require('../../../images/1stDragable.jpg')}>
-                </motion.img>
-                <motion.img 
-                    drag="x" 
+                    drag="x y" 
                     dragControls={controls}
                     className='draggable-cards' 
                     src={require('../../../images/1stDragable.jpg')}>
