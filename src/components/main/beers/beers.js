10eedcc9f2a39bc0cfcf36291c42cd5aa05c8ffa
thayorn <thayornswordsman@gmail.com>
2023-12-03 18:00:58 +0300

    beeers section

--- src/components/main/beers/beers.js
+++ src/components/main/beers/beers.js
@@ -1,8 +1,54 @@
 import { motion } from "framer-motion";
+
 import './beers.css'
+  
+  const cardVariants = {
+    offscreen: {
+      y: 300
+    },
+    onscreen: {
+      y: 50,
+      rotate: -10,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 0.8
+      }
+    }
+  };
+  
+  const hue = (h) => `hsl(${h}, 100%, 50%)`;
+  
+  function Card({ emoji, hueA, hueB }) {
+    const background = `linear-gradient(306deg, ${hue(hueA)}, ${hue(hueB)})`
+  
+    return (
+      <motion.div
+        className="card-container"
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+      >
+        <div className="splash" style={{ background }} />
+        <motion.div className="card" variants={cardVariants}>
+          {emoji}
+        </motion.div>
+      </motion.div>
+    );
+  }
+  
+  const beers = [
+    ["🍅", 340, 10],
+    ["🍊", 20, 40],
+    ["🍋", 60, 90],
+    ["🍐", 80, 120],
+    ["🍏", 100, 140],
+    ["🫐", 205, 245],
+    ["🍆", 260, 290],
+    ["🍇", 290, 320]
+  ]
 
 const Beers = () => {
-
 
     return (
         <motion.div className="beers-wrapper"
@@ -12,6 +58,13 @@ const Beers = () => {
             exit={{ y: -100, opacity: 0 }}
             transition={{ duration: 0.6}}
         >
+            {
+                beers.map(([emoji, hueA, hueB]) =>
+                    (
+                        <Card emoji={emoji} hueA={hueA} hueB={hueB} key={emoji} />
+                    )
+                )
+            }
         </motion.div>
     )
 }
